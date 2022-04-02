7f88f54ebeee6d86f2d7256ffc852b0253396930
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-04-02 09:42:02 +0200

    mb: practice updated

--- src/main/java/hu/progmatic/Practice_20220402_02.java
+++ src/main/java/hu/progmatic/Practice_20220402_02.java
@@ -1,5 +1,64 @@
 package hu.progmatic;
 
 public class Practice_20220402_02 {
-    
+    /**
+     * Az adott kerülethez tartozó történelmi adatokat visszaadó metódus.
+     * @param district a kerület száma (I. = 1)
+     * @return az első 4 hét hőmérsékletei
+     */
+    public static int[] getTemperatureData(int district) {
+        return null;
+    }
+
+    /**
+     * Az első 4 hét legmagasabb hőmérsékletét visszaadó metódus.
+     * @param district a kerület száma (I. = 1)
+     * @return a legmagasabb hőmérséklet
+     */
+    public static int getMaxTemperatureByDistrict(int district) {
+        return 0;
+    }
+
+    /**
+     * Az első 4 hét legalacsonyabb hőmérsékletét visszaadó metódus.
+     * @param district a kerület száma (I. = 1)
+     * @return a legalacsonyabb hőmérséklet
+     */
+    public static int getMinTemperatureByDistrict(int district) {
+        return 0;
+    }
+
+    /**
+     * Az első 4 hét során melyik körzetben mérték a legmagasabb hőmérsékletet?
+     * @return körzet száma
+     */
+    public static int getWarmestDistrict() {
+        return 0;
+    }
+
+    /**
+     * Az első 4 hét során melyik körzetben mérték a legalacsonyabb hőmérsékletet?
+     * @return körzet száma
+     */
+    public static int getColdestDistrict() {
+        return 0;
+    }
+
+    /**
+     * Az első 4 hét során hány fagyos nap (hőmérséklet < 0) volt a megadott körzetben?
+     * @param district a kerület száma (I. = 1)
+     * @return fagyos napok számq
+     */
+    public static int getFreezyDays(int district) {
+        return 0;
+    }
+
+    /**
+     * Az első 4 hét során hány forró nap (hőmérséklet >= 25) volt a megadott körzetben?
+     * @param district a kerület száma (I. = 1)
+     * @return forró napok száma
+     */
+    public static int getWarmDays(int district) {
+        return 0;
+    }
 }
