7c6af8dd73dedf578be0e9a3ddc6219195c23ee1
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-04-02 10:43:13 +0200

    mb: practice updated

--- src/main/java/hu/progmatic/Practice_20220405_05.java
+++ src/main/java/hu/progmatic/Practice_20220405_05.java
@@ -3,11 +3,35 @@ package hu.progmatic;
 public class Practice_20220405_05 {
     /**
      * Az első olyan napot adja vissza, amelyik melegebb volt a megadott hőmérsékletnél.
+     * Nem csak az első 4 hetet kell figyelembe vennie.
      * @param district a kerület száma (I. = 1)
      * @param temperature referencia hőmérséklet
-     * @return
+     * @return első talált nap indexe
      */
     public static int getFirstDayWarmerThan(int district, int temperature) {
+        return -1;
+    }
+
+    /**
+     * Az első olyan napot adja vissza, amelyik hidegebb volt a megadott hőmérsékletnél.
+     * Nem csak az első 4 hetet kell figyelembe vennie.
+     * @param district a kerület száma (I. = 1)
+     * @param temperature referencia hőmérséklet
+     * @return első talált nap indexe
+     */
+    public static int getFirstDayColderThan(int district, int temperature) {
+        return -1;
+    }
+
+    /**
+     * Az első olyan napot visszaadó metódus, amikor a kezdőnaptól fogva összesen leesett csapadék eléri a megadott szintet.
+     * Nem csak az első 4 hetet kell figyelembe vennie.
+     * @param district a kerület száma (I. = 1)
+     * @param dayFrom 2250.01.01. óta eltelt napok száma
+     * @param precipitation összes csapadék
+     * @return első olyan nap indexe, amikor az összesen leesett csapadék eléri a megadott értéket
+     */
+    public static int getPrecipitationTotalAtLeast(int district, int dayFrom, int precipitation) {
         return -1;
     }
 }
