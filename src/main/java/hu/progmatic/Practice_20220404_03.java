10c1a4ea9ca326bd6a6d6dd2e21adc51b1bc4704
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-04-04 18:59:06 +0200

    mb: practice updated

---
+++ src/main/java/hu/progmatic/Practice_20220404_03.java
@@ -0,0 +1,20 @@
+package hu.progmatic;
+
+public class Practice_20220404_03 {
+    /**
+     * Egy dolgozó maximum maxForce erőt képes kifejteni (optimális szög esetében)
+     * A ténylegesen kifejtett erőt a sin(szög) * maxForce képlettel kapjuk meg.
+     * A kezdő szög 0 radián.
+     * Írjunk metódust, amelyik visszaadja,
+     * hogy hányszor kell 0.01 radiánnal megnövelni a szöget,
+     * hogy elérjük a szükséges erőt (requiredForce).
+     * Ha requiredForce > maxForce, akkor térjünk vissza -1-el.
+     *
+     * @param maxForce
+     * @param requiredForce
+     * @return
+     */
+    public static int getMinimumTurns(int maxForce, int requiredForce) {
+        return 0;
+    }
+}
