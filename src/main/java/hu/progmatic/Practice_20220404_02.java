37283f95f269194e147879b6b84361835213189a
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-04-04 17:33:46 +0200

    mb: practice updated

---
+++ src/main/java/hu/progmatic/Practice_20220404_02.java
@@ -0,0 +1,17 @@
+package hu.progmatic;
+
+public class Practice_20220404_02 {
+    /**
+     * Van 3 különböző magasságú dobozunk.
+     * Lehet-e belőlük egy vízszintes felületet létrehozni az alábbi 2 módszer egyikével:
+     * - egymás mellé rakjuk őket
+     * - kettőt egymásra rakunk
+     * @param a magasság
+     * @param b magasság
+     * @param c magasság
+     * @return lehet-e vízszintes felületet létrehozni?
+     */
+    public static boolean doBoxesFit(int a, int b, int c) {
+        return false;
+    }
+}
