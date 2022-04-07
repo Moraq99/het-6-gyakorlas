6bbc39c8a108864366bbb919a964811d0e4d0fe4
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-04-07 18:58:19 +0200

    mb: practice updated

---
+++ src/main/java/hu/progmatic/Practice_20220407_06.java
@@ -0,0 +1,27 @@
+package hu.progmatic;
+
+public class Practice_20220407_06 {
+    public static void main(String[] args) {
+        // FLAG
+        // 11111b = 31
+        // 01111b = 15
+        // 01011b = 11
+        // piros fények (ki/be) +1
+        // zöld fények (ki/be) +2
+        // kék fények (ki/be) +4
+        // füst (ki/be) +8
+        // hab (ki/be) +16
+    }
+
+    public boolean isFoamOn(int flags) {
+        return false;
+    }
+
+    public int setBlueOn(int flags) {
+        return 0;
+    }
+
+    public int setBlueOff(int flags) {
+        return 0;
+    }
+}
