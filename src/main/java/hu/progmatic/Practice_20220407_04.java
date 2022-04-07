d773f1d43890560c9c512e4ec571f485ec0cf7f8
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-04-07 18:27:43 +0200

    mb: practice updated

--- src/main/java/hu/progmatic/Practice_20220407_04.java
+++ src/main/java/hu/progmatic/Practice_20220407_04.java
@@ -8,6 +8,26 @@ public class Practice_20220407_04 {
         System.out.println(isArrayMirror(new int[] {1, 2, 2, 1}));
         System.out.println(isArrayMirror(new int[] {1, 1, 2}));
         System.out.println(isArrayMirror(new int[] {1, 2, 3}));
+
+        int[] numbers = {1, 2, 3, 4};
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
+        System.out.println();
+
+        int billnr = 2;
+        System.out.println("MB/2022/" + billnr);
+        System.out.println("MB/2022/%06d");
+        System.out.printf("MB/2022/%06d\n", billnr);
+
+        billnr = 1155;
+        System.out.println("MB/2022/" + billnr);
+        System.out.printf("MB/2022/%06d\n", billnr);
+        double temperature = 2.0 / 3.0;
+        System.out.println(temperature);
+        System.out.printf("%.2f\n", temperature);
     }
 
     public static boolean isArrayMirror(int[] numbers) {
